8ec98786ec478abcf6134e85d9078f70b299a185
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-04-06 11:50:10 +0100

    Added negative path

--- src/Controllers/NetflixController.php
+++ src/Controllers/NetflixController.php
@@ -4,16 +4,19 @@ namespace OdmflixApi;
 
 require_once __DIR__ . '/../lib/includes.php';
 
-class ShowController extends ControllerBase
+class NetflixController extends ControllerBase
 {
-	public function __construct(private ShowRepository $showRepo, private ControllerContext $ctx) {
+	public function __construct(private ShowRepository $showRepo, ControllerContext $ctx) {
 
 		parent::__construct($ctx);
 	}
 
 	public function Show(int $id): IResult
 	{
-		$show = $this->showRepo->GetShowById($id);
-		return $this->Ok($show);
+		if($show = $this->showRepo->GetShowById($id)) {
+			return $this->Ok($show);
+		}
+
+		return $this->NotFound();
 	}
 }
