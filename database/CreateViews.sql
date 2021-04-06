95a975b3889f61ce66804bab3b6472bac1942e04
Hugo Woodiwiss <hj.woodiwiss@gmail.com>
2021-04-06 11:28:43 +0100

    Added script to create views

---
+++ database/CreateViews.sql
@@ -0,0 +1,16 @@
+-- Full Shows View
+CREATE OR REPLACE VIEW `vw_Shows`
+AS
+SELECT 
+	`Shows`.`Id` AS `Id`,
+	`ShowTypes`.`Name` AS `ShowType`,
+	`Shows`.`Title` AS `Title`,
+	`Shows`.`DateAdded` AS `DateAdded`,
+	`ReleaseYears`.`Year` AS `ReleaseYear`,
+	`Ratings`.`Name` AS `Rating`,
+	`Shows`.`Duration` AS `Duration`,
+	`Shows`.`Description` AS `Description`
+FROM `Shows`
+JOIN `ShowTypes` ON `Shows`.`ShowTypeId` = `ShowTypes`.`Id`
+JOIN `ReleaseYears` ON `Shows`.`ReleaseYearId` = `ReleaseYears`.`Id`
+LEFT JOIN `Ratings` ON `Shows`.`RatingId` = `Ratings`.`Id`;
